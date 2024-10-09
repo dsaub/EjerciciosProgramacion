7d3a973a824f690c8285739382128b26f7883745
Daniel <danilacasito8@gmail.com>
2024-10-09 14:23:22 +0200

    Updated GUI to at least open it

--- src/main/java/me/elordenador/megajar/tema3/tresenraya/TresEnInterfaz.java
+++ src/main/java/me/elordenador/megajar/tema3/tresenraya/TresEnInterfaz.java
@@ -1,38 +1,27 @@
 package me.elordenador.megajar.tema3.tresenraya;
 
 
-import me.elordenador.megajar.tema3.tresenraya.gui.Boton;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.awt.*;
+public class TresEnInterfaz extends Application {
 
-public class TresEnInterfaz {
-    GridLayout layout;
-    JFrame frame;
-    Boton[][] boton;
-    public static void main(String[] args) {
-        TresEnInterfaz instance = new TresEnInterfaz();
-        instance.show();
+
+    public static void main(String[] args)  {
+        launch(args);
     }
 
-    public TresEnInterfaz() {
-        frame = new JFrame("Tres en Raya");
-        initialize();
-    }
-    public void initialize() {
-        boton = new Boton[3][3];
-        layout = new GridLayout(3,3);
-        for (int x = 0; x <= 2; x++) {
-            for (int y = 0; y <= 2; y++) {
-                boton[x][y] = new Boton(x,y," ");
-                frame.add(boton[x][y]);
-            }
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/me/elordenador/megajar/tema3/tresenraya/main.fxml"));
 
+        Scene scene = new Scene(root, 300, 275);
 
-        frame.setLayout(layout);
-    }
-    public void show() {
-        frame.setVisible(true);
+        stage.setTitle("Tres en Raya");
+        stage.setScene(scene);
+        stage.show();
     }
 }
